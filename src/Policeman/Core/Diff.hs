9b777b92c4e8c171b9916933af8546d475b99714
Veronika Romashkina <vrom911@gmail.com>
2020-01-25 16:40:36 +0000

    [#7] Implement basic Diff Evaluation (#18)
    * [#7] Implement basic Diff Evaluation
    
    Resolves #7
    
    * Fix

--- src/Policeman/Core/Diff.hs
+++ src/Policeman/Core/Diff.hs
@@ -4,9 +4,14 @@
 module Policeman.Core.Diff
     ( PackageDiff (..)
     , Diff (..)
+    , emptyDiff
+    , hasDiffAdded
+    , hasDiffDeleted
     ) where
 
 import Policeman.Core.Package (Export, Module)
+
+import qualified Data.Set as Set
 
 
 data PackageDiff = PackageDiff
@@ -17,4 +22,16 @@ data PackageDiff = PackageDiff
 data Diff a = Diff
     { diffAdded   :: !(Set a)
     , diffDeleted :: !(Set a)
+    } deriving stock (Eq)
+
+emptyDiff :: Diff a
+emptyDiff = Diff
+    { diffAdded   = Set.empty
+    , diffDeleted = Set.empty
     }
+
+hasDiffDeleted :: Diff a -> Bool
+hasDiffDeleted = not . Set.null . diffDeleted
+
+hasDiffAdded :: Diff a -> Bool
+hasDiffAdded = not . Set.null . diffAdded
--- src/Policeman/Core/Package.hs
+++ src/Policeman/Core/Package.hs
@@ -3,7 +3,15 @@
 
 module Policeman.Core.Package
     ( PackageName (..)
+
+      -- * Versioning
     , Version (..)
+    , versionToText
+      -- ** Bumping versions
+    , bumpMarketingMajorVersion
+    , bumpMajorVersion
+    , bumpMinorVersion
+    , bumpTinyMinorVersion
 
       -- * Package Structure
     , PackageStructure (..)
@@ -13,6 +21,8 @@ module Policeman.Core.Package
     , ModuleStructure (..)
     ) where
 
+import qualified Data.Text as Text
+
 
 {- | Package name, like @relude@.
 -}
@@ -20,9 +30,59 @@ newtype PackageName = PackageName
     { unPackageName :: Text
     }
 
-newtype Version = Version
-    { unVersion :: Text
+{- | Package version that should satisfy the PVP.
+<https://pvp.haskell.org/>
+-}
+data Version = Version
+    { versionA :: !Int  -- ^ Marketing major.
+    , versionB :: !Int  -- ^ Major.
+    , versionC :: !Int  -- ^ Minor.
+    , versionD :: !Int  -- ^ Tiny minor.
     }
+
+nullVersion :: Version
+nullVersion = Version
+     { versionA = 0
+     , versionB = 0
+     , versionC = 0
+     , versionD = 0
+     }
+
+{- | Shows 'Version' in the standard format:
+
+@
+A.B.C.D
+1.0.2.1
+@
+-}
+versionToText :: Version -> Text
+versionToText Version{..} = Text.intercalate "." $ map show
+    [ versionA
+    , versionB
+    , versionC
+    , versionD
+    ]
+
+bumpMarketingMajorVersion :: Version -> Version
+bumpMarketingMajorVersion Version{..} = nullVersion
+    { versionA = versionA + 1 }
+
+bumpMajorVersion :: Version -> Version
+bumpMajorVersion Version{..} = nullVersion
+    { versionA = versionA
+    , versionB = versionB + 1
+    }
+
+bumpMinorVersion :: Version -> Version
+bumpMinorVersion Version{..} = nullVersion
+    { versionA = versionA
+    , versionB = versionB
+    , versionC = versionC + 1
+    }
+
+bumpTinyMinorVersion :: Version -> Version
+bumpTinyMinorVersion ver@Version{..} = ver
+    { versionD =  versionD + 1 }
 
 {- | Overall structure of the package.
 Generally speaking, this is the resulting data type of custom parsing the
